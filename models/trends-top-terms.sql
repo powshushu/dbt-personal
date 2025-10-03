e32732076bd0e51dfd1250532b4473c831f99d7f
ps.carreon@gmail.com <ps.carreon@gmail.com>
2025-10-03 00:51:05 +0000

    first commit

--- models/trends-top-terms.sql
+++ models/trends-top-terms.sql
@@ -1,15 +1,23 @@
--- This query shows a list of the daily top Google Search terms.
+/*
+{{
+  config(
+    materialized='view'
+  )
+}}
+*/
+
 SELECT
-   refresh_date AS Day,
-   term AS Top_Term,
-       -- These search terms are in the top 25 in the US each day.
-   rank,
+   refresh_date
+   , term top_term
+   , rank
 FROM `bigquery-public-data.google_trends.top_terms`
 WHERE
-   rank = 1
-       -- Choose only the top term each day.
-   AND refresh_date >= DATE_SUB(CURRENT_DATE(), INTERVAL 2 WEEK)
-       -- Filter to the last 2 weeks.
-GROUP BY Day, Top_Term, rank
-ORDER BY Day DESC
-   -- Show the days in reverse chronological order.
+   rank <= 5 --top terms
+   AND refresh_date >= DATE_SUB(CURRENT_DATE(), INTERVAL 2 WEEK) --last 2 weeks
+GROUP BY
+   refresh_date
+   , term
+   , rank
+ORDER BY 
+    refresh_date DESC
+    , rank
